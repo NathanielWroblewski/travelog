f69068bf4840dfc056d3a10702a6245fdd82ffc0
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2014-02-02 19:23:20 -0800

    Set up cloudfront

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def cloudfront(url)
+    s3_url = "s3.amazonaws.com/#{ENV['S3_BUCKET']}"
+    cloudfront_url = "#{ENV['CLOUDFRONT_DOMAIN']}.cloudfront.net"
+    url.gsub(s3_url, cloudfront_url)
+  end
 end
